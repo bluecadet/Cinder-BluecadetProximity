b78cc10e21430f0fecb2a5890f9ad4ffd7b7d74b
Weili Shi <me@shi-weili.com>
2016-08-22 18:04:46 -0400

    Modified the API; implemented it. The code builts.

--- src/Proximity.h
+++ src/Proximity.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "cinder/app/App.h"
+#include "cinder/Serial.h"
 
 namespace bluecadet {
 namespace proximity {
@@ -9,24 +10,17 @@ typedef std::shared_ptr<class Proximity> ProximityRef;
 class Proximity {
 public:
 	struct Device {
-		std::string vid;
-		std::string pid;
-		std::string serial;
+		cinder::SerialRef serial;
+		std::string name;
+		std::string path;
 		enum class Status {
 			CONNECTED,
 			DISCONNECTED
 		} status;
 		int distance;
-		//const int minDistance = 300;
-		//const int maxDistance = 5000;
-		//bool hasDetectedObject();
 	};
 
-	static ProximityRef getInstance() {
-		static ProximityRef instance = nullptr;
-		if (!instance) instance = ProximityRef(new Proximity());
-		return instance;
-	}
+	static ProximityRef getInstance();
 
 	Proximity();
 	~Proximity();
@@ -34,13 +28,16 @@ public:
 	void start();
 	void stop();
 
-	size_t getNumDevices() const { return mDevices.size(); };
-	const Device& getDevice(const std::string &serial) const;
-	const vector<Device>& getAllDevices() const;
+	const Device& getDevice(const std::string &sn) const;
+	const std::map<std::string, Device>& getAllDevices() const;
 
 private:
-	cinder::signals::Signal<void(const Device&)> mDeviceConncted;
-	vector<Device> mDevices;
+	std::map<std::string, Device> mDevices;
+	cinder::signals::Connection mSignalsConnection;
+
+	static const double UPDATE_INTERVAL;
+	double lastUpdateTime = -UPDATE_INTERVAL;
+
 	void updateDeviceList();
 	void updateData();
 }; // class Proximity
